2081f35855fc228e6172f79999d8fccea112e455
Lucky7649 <luckypaswan745@gmail.com>
2025-08-31 21:03:46 +0530

    log in , log out , register form craeted and all urls set up

--- Luckytweet/Luckytweet/urls.py
+++ Luckytweet/Luckytweet/urls.py
@@ -19,8 +19,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include
+from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tweet', include('tweet.urls')),
+    path('tweet/', include('tweet.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
